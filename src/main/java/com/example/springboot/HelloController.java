63c47d9f71acefbfdec60faaf45728f49e35af14
djptek <dominic.page@elastic.co>
2020-02-10 17:30:26 +0000

    added BP

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import co.elastic.apm.api.ElasticApm;
+import co.elastic.apm.api.Transaction;
+import co.elastic.apm.api.Span;
+
 @RestController
 // implement command line runner
 public class HelloController {
@@ -18,15 +22,26 @@ public class HelloController {
 		StringBuilder builder = new StringBuilder();
 		builder.append("DB values:");
 		jdbcTemplate.query(
-        		"SELECT my_int FROM text", 
+        		"SELECT my_int FROM test", 
         		(rs, rowNum) -> new TestDBRow(rs.getInt("my_int"))
                	).forEach(testDBRow -> {
-			builder.append("<br>").append(testDBRow.toString());
-			jdbcTemplate.query(
-        			"SELECT COUNT(my_int) FROM test WHERE my_int = ?", 
-				new Object[] { testDBRow.my_int() },
-				(subRs, subRowNum) -> subRs.getInt("count")
-			).forEach(count -> builder.append("-").append(count.toString()));
+			Transaction transaction = ElasticApm.startTransaction();
+			try {
+    				transaction.setType(Transaction.TYPE_REQUEST);
+    				transaction.setName("HelloController#Java8forEach:"+testDBRow.toString());
+				builder.append("<br>").append(testDBRow.toString());
+				jdbcTemplate.query(
+        				"SELECT COUNT(my_int) FROM test WHERE my_int = ?", 
+					new Object[] { testDBRow.my_int() },
+					(subRs, subRowNum) -> subRs.getInt("count")
+				).forEach(count -> builder.append("-").append(count.toString()));
+			} catch (Exception e) {
+    				transaction.captureException(e);
+    				throw e;
+			} finally {
+    				transaction.setResult("OK");
+    				transaction.end();
+			}
 		});
 		return builder.toString();
 	}
