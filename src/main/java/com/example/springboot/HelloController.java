fdabd5c2269167a61ba97d08bb96770be60e3f2d
djptek <dominic.page@elastic.co>
2020-04-07 17:22:47 +0000

    added distributed tracing

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 // add jdbc Template
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,6 +11,8 @@ import co.elastic.apm.api.ElasticApm;
 import co.elastic.apm.api.Transaction;
 import co.elastic.apm.api.Span;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 // implement command line runner
 public class HelloController {
@@ -17,15 +20,20 @@ public class HelloController {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
+	//public String index(@RequestHeader("elastic-apm-traceparent") String traceparent) {
+	//public String index() {
 	@RequestMapping("/")
-	public String index() {
+	//public String index(@RequestHeader("traceparent") String traceparent) {
+	public String index(HttpServletRequest request) {
 		StringBuilder builder = new StringBuilder();
 		builder.append("DB values:");
 		jdbcTemplate.query(
         		"SELECT my_int FROM test", 
         		(rs, rowNum) -> new TestDBRow(rs.getInt("my_int"))
                	).forEach(testDBRow -> {
-			Transaction transaction = ElasticApm.startTransaction();
+			//Transaction transaction = ElasticApm.startTransaction();
+			Transaction transaction = ElasticApm.startTransactionWithRemoteParent(
+		    		traceparent -> request.getHeader("traceparent"));
 			try {
     				transaction.setType(Transaction.TYPE_REQUEST);
     				transaction.setName("HelloController#Java8forEach:"+testDBRow.toString());
